01f5c025863f5fd68726e4aebb4a1cb71877387f
Hardik Pratap <hardikpratap188@gmail.com>
2024-10-13 05:10:14 +0530

    Html Css Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,9 +29,27 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== SWIPER CAR ===============*/
-    
+const swiper = new Swiper('.home__swiper', {
+    speed: 1200,
+    effect: 'fade',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        // Add leading zero to index and make it 2 digits (e.g., 01, 02)
+        return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + '</span>';
+      },
+    },
+  });
 
 /*=============== GSAP ANIMATION ===============*/
 
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader=()=>{
+    const header=document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                        : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll',blurHeader)
